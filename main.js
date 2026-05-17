12530584b1680d4954c81d8391d9f26f5e8d213a
IllesSigmaHunor <illes.s.hunor@gmail.com>
2026-05-17 18:59:43 +0200

    js code button aper(nwk)

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+const select = document.getElementById("group");
+const row = document.getElementById("fradi")
+const rix = document.getElementById("misi")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+    row.classList.add("col-6")
+    rix.classList.add("col-6");
+
+    const selectedValue = select.value;
+    const buton = document.createElement("button");
+    buton.textContent = selectedValue;
+    row.append(buton);
+    buton.onclick = function(){
+        window.open(
+            `https://hu.wikipedia.org/wiki/${selectedValue}`,
+            "_blank"
+        );
+    }
+
+});
